a16f710ba26b1b30101a1e6337f88cbc34e7d876
DableUTeeF <palm22180@gmail.com>
2023-04-14 21:28:55 +0700

    CLIP laptop runs

--- sentence_train.py
+++ sentence_train.py
@@ -6,7 +6,8 @@ from PIL import Image
 import os
 import torch
 from utils import test_map
-from torch.utils.tensorboard import SummaryWriter
+import functools
+# from torch.utils.tensorboard import SummaryWriter
 
 
 class TIPCBEvaluator(evaluation.SentenceEvaluator):
@@ -33,10 +34,14 @@ class TIPCBEvaluator(evaluation.SentenceEvaluator):
             query_label = torch.cat((query_label, label))
             gallery_label = torch.cat((gallery_label, label))
         cm0, cm4, cm9, ap = test_map(query_feature, query_label, gallery_feature, gallery_label)
-        self.writer.add_scalar('Metrics/r@1', cm0, epoch)
-        self.writer.add_scalar('Metrics/r@5', cm4, epoch)
-        self.writer.add_scalar('Metrics/r@10', cm9, epoch)
-        self.writer.add_scalar('Metrics/ap', ap, epoch)
+        if output_path is not None:
+            with open(os.path.join(output_path, 'output.csv'), 'a') as wr:
+                wr.write(f'{cm0},{cm4},{cm9},{ap}\n')
+        if self.writer is not None:
+            self.writer.add_scalar('Metrics/r@1', cm0, epoch)
+            self.writer.add_scalar('Metrics/r@5', cm4, epoch)
+            self.writer.add_scalar('Metrics/r@10', cm9, epoch)
+            self.writer.add_scalar('Metrics/ap', ap, epoch)
         return ap
 
 
@@ -47,8 +52,7 @@ if __name__ == '__main__':
     parser.add_argument('--batch_size', type=int, default=16)
     parser.add_argument('--num_epochs', type=int, default=45)
     parser.add_argument('--device', type=str, default='cuda')
-    parser.add_argument('--log_dir', type=str, default='./log')
-    parser.add_argument('--output_path', type=str, default='/media/palm/Data/tipcb/checkpoint/remove77_2')
+    parser.add_argument('--output_path', type=str, default='checkpoint/remove77_2')
 
     # optimizer
     parser.add_argument('--lr', type=float, default=1e-4)
@@ -60,18 +64,20 @@ if __name__ == '__main__':
     parser.add_argument('--model', type=str, default='clip-ViT-B-32')
 
     # dataset
-    parser.add_argument("--text_vector_path", type=str, default="/media/palm/Data/tipcb/text_vectors")
-    parser.add_argument("--image_root_path", type=str, default="/media/palm/BiggerData/caption/CUHK-PEDES/CUHK-PEDES/imgs")
-    parser.add_argument("--json_path", type=str, default="/media/palm/BiggerData/caption/CUHK-PEDES/CUHK-PEDES/caption_all_encn.json")
+    parser.add_argument("--image_root_path", type=str, default="CUHK-PEDES/imgs")
+    parser.add_argument("--json_path", type=str, default="CUHK-PEDES/caption_all.json")
     parser.add_argument('--width', type=int, default=128)
     parser.add_argument('--height', type=int, default=384)
+    parser.add_argument('--embed_dim', type=int, default=512)
     parser.add_argument('--regen', type=bool, default=False)
+    
 
     args = parser.parse_args()
     model = SentenceTransformer(args.model)
     tokenizer = model._first_module().processor.tokenizer
 
     train_list, val_list = split(args)
+    val_list = functools.reduce(lambda a, b: a + b, val_list)
 
     train_dataset = SentenseDataset(args, train_list, tokenizer)
     # val_dataset = SentenseDataset(args, val_list, tokenizer)
@@ -80,8 +86,8 @@ if __name__ == '__main__':
     train_dataloader = DataLoader(train_dataset, shuffle=True, batch_size=args.batch_size, collate_fn=SentenseDataset.collate_fn)
     train_loss = losses.CosineSimilarityLoss(model)
     # evaluator = evaluation.EmbeddingSimilarityEvaluator.from_input_examples(val_dataset)
-    writer = SummaryWriter(log_dir=f'{args.output_path}/log')
-    evaluator = TIPCBEvaluator(val_list, writer)
+    # writer = SummaryWriter(log_dir=f'{args.output_path}/log')
+    evaluator = TIPCBEvaluator(val_list, writer=None)
 
     model.fit(train_objectives=[(train_dataloader, train_loss)], epochs=args.num_epochs,
               warmup_steps=args.warmup_steps, evaluator=evaluator,
